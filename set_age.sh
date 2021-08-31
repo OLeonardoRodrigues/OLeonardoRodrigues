655043073f671f7d86828e15e7765bbda9eb72a7
Leonardo Rodrigues de Oliveira <84041478+OLeonardoRodrigues@users.noreply.github.com>
2021-08-31 15:20:18 -0300

    Update set_age.sh

--- set_age.sh
+++ set_age.sh
@@ -3,4 +3,4 @@
 birthday="1999-12-27"
 new_age=$(( ($(date -u +%s) - $(date -u -d $birthday +%s)) / 31579200 ))
 
-sed -i -e "s|<span id='age'>[[:digit:]]\+</span>|<span id='age'>${new_age}</span>|g" README.md
+sed -r -i -e 's|<span id="age">([0-9]{0,})</span>|<span id="age">'"${new_age}"'</span>|g' README.md
